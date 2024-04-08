c4c7ec0e6701df6128caffa7790af5ad822fea5f
Sebastien <bodin.seb@laposte.net>
2024-04-08 13:35:56 +0200

    Classe article modifiee et enum etat_vente

--- src/main/java/fr/eni/ecole/enchereseniprojetbackend/bo/Article.java
+++ src/main/java/fr/eni/ecole/enchereseniprojetbackend/bo/Article.java
@@ -1,13 +1,37 @@
 package fr.eni.ecole.enchereseniprojetbackend.bo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+//@Entity
 public class Article {
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nomArticle;
+    private String description;
+    private Date dateDebut;
+    private Date dateFin;
+    private long miseAPrix;
+    private EtatVente prixVente;
+
+    private Categorie categorie;
+    private Utilisateur vendeur;
+    private Utilisateur acheteur;
+    private List<Enchere> encheres;
+    private Retrait retrait;
+
+
 }
---
+++ src/main/java/fr/eni/ecole/enchereseniprojetbackend/bo/EtatVente.java
@@ -0,0 +1,8 @@
+package fr.eni.ecole.enchereseniprojetbackend.bo;
+
+public enum EtatVente {
+    CREER,
+    EN_COURS,
+    VENDU,
+    RETIRE
+}
