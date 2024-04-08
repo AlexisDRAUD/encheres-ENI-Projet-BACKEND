00da3ecef48a0a25561fc30251df99b57ebb1d10
Sebastien <bodin.seb@laposte.net>
2024-04-08 12:44:31 +0200

    Classe article

--- src/main/java/fr/eni/ecole/enchereseniprojetbackend/bo/Article.java
+++ src/main/java/fr/eni/ecole/enchereseniprojetbackend/bo/Article.java
@@ -1,4 +1,13 @@
 package fr.eni.ecole.enchereseniprojetbackend.bo;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Article {
 }
