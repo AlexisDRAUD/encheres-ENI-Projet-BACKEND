33a3a0b7f2682e604eb61201402fa1f58475d6ae
Alexis DRAUD <alexis.draud2023@campus-eni.fr>
2024-04-08 10:31:51 +0200

    First commit

---
+++ src/main/java/org/example/enchereseniprojetbackend/EncheresEniProjetBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.enchereseniprojetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncheresEniProjetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncheresEniProjetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/enchereseniprojetbackend/EncheresEniProjetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.enchereseniprojetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncheresEniProjetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
